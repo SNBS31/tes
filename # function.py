b572a274d65e309e34a254358539606a27f0de4e
Salle-Njume <sallebrian2019@gmail.com>
2023-02-22 08:53:31 -1100

    Salle's assignment

---
+++ # function.py
@@ -0,0 +1,9 @@
+# function = a block of code executed only when it is called.
+
+def repeat(name):
+    print("I'm blessed in in the name of "+name)
+    print("Amen!")
+    
+repeat("Jesus")
+repeat("Jesus")
+repeat("Jesus")
